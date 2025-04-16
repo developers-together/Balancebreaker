8fae8ac2b9ffa88e39baa3ff77a9469b3daf9d31
Shehab <saohm56@gmail.com>
2025-04-16 20:58:29 +0200

    removed character selector

--- GameManager.gd
+++ GameManager.gd
@@ -1,15 +1,4 @@
 extends Node
 
 
-var character: String 
-
-func UseRX42():
-	
-	character = "RX-42"
-	
-func UseVX09():
-	
-	character = "VX-09"
-
-func GetCharacter():
-	return character
+var selected_character: PackedScene = null
